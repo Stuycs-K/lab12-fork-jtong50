6ad54d0f741253fabd0b7bc036dee0b158887d04
Janet Tong <jtong50@stuy.edu>
2024-11-25 15:35:19 -0500

    children made, pid printed

--- fork.c
+++ fork.c
@@ -10,5 +10,24 @@
 #include <dirent.h>
 
 int main(){
+  printf("%d about to create 2 child processes.\n", getpid());
+
+  pid_t p1;
+  p1 = fork();
+  if (p1 < 0){
+    perror("forkfail");
+    exit(1);
+  } else if (p1 == 0){
+
+    printf("%d %d\n", getpid());
+
+  }
+  else{
+    pid_t p2;
+    p2 = fork();
+    if (p2 == 0){
+      printf("%d\n", getpid());
+    }
+  }
 
 }
